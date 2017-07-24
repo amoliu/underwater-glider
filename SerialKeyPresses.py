1c11b8a9e31fa23b011d6dd89693ca10e81d0f7c
Oliver Williams <oliw2150@gmail.com>
2017-07-24 11:54:38 +0100

    Update SerialKeyPresses.py

--- SerialKeyPresses.py
+++ SerialKeyPresses.py
@@ -2,16 +2,19 @@ import serial;
 from tkinter import *;
 serial_commands = {"a" : 1, "d" : 2, "w" : 3, "s" : 4, "i" : 5, "k" : 6};
 
-def keyup(e):
+def keyUp(e):
     if e.char in serial_commands.keys():
         ser.write(bytes([serial_commands[e.char]]));
-        
 
-root = Tk();
+def initialise():
+    try:
+        ser = serial.Serial('/dev/ttyUSB0');
+        ser.baudrate = 115200;
+    except Exception as e:
+        raise Exception(e);
 
-root.bind("<KeyRelease>", keyup);
-try:
-    ser = serial.Serial('/dev/ttyUSB1');
-    ser.baudrate = 115200;
-except:
-    print("The program was unable to create a connection with the serial port");
+    root = Tk();
+    root.bind("<KeyRelease>", keyUp);
+
+if __name__ == "__main__":
+    initialise();
